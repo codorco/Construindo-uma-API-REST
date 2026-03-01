22939dd56794834e5b7225367e5d9437cb0cd86a
codorco <william.emerson@hotmail.com>
2026-02-28 23:04:06 -0300

    Implementa a classe Response e adiciona configuração inicial da API

---
+++ _inc/config.sample.php
@@ -0,0 +1,13 @@
+<?php
+
+define('API_VERSION', '1.0.0');
+
+// api enable or disable
+define('API_ACTIVE', true);
+define('API_MENSAGE', 'API esta em manutenção.');
+
+// mysql config
+define('MYSQL_HOST',            '');
+define('MYSQL_DATABASE',        '');
+define('MYSQL_USER',            '');
+define('MYSQL_PASS',            '');
